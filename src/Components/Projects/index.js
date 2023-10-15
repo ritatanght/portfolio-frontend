2a6d729f23e96e3b2dbccbaa71ca2e94b60dad1b
ritatanght <doggymint@gmail.com>
2023-10-15 18:05:31 -0400

    add sanity check before map

--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -1,20 +1,21 @@
-import "./projects.scss";
-import { useState, useEffect } from "react";
+import "./projects.scss";import { useState, useEffect } from "react";
 import Project from "./Project";
 import { getAll } from "../../service/service.js";
 import BarLoader from "react-spinners/BarLoader";
 
 const Projects = () => {
   const [projects, setProjects] = useState([]);
+
   useEffect(() => {
     getAll()
       .then((projects) => setProjects(projects))
       .catch((err) => console.log(err));
   }, []);
 
-  const projectsElements = projects.map((project) => (
-    <Project key={project._id} project={project} />
-  ));
+  const projectsElements =
+  Array.isArray(projects) &&
+  projects.map((project) => <Project key={project._id} project={project} />);
+
   return (
     <section className="projects-page">
       <h2 className="text-center">Projects</h2>
