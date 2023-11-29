94290efe0c68f1ac1b1cfbca109b120d50fc3bb6
ritatanght <doggymint@gmail.com>
2023-11-28 22:45:00 -0500

    Add photos and more skills to data

--- src/data.js
+++ src/data.js
@@ -7,32 +7,37 @@ import {
   DiDjango,
   DiReact,
 } from "react-icons/di";
-import { TbDatabase } from "react-icons/tb";
 import {
   SiTypescript,
   SiExpress,
   SiMongodb,
   SiTailwindcss,
+  SiPostgresql,
+  SiAdobe,
 } from "react-icons/si";
+import photo from "./assets/images/profile_pic.jpg";
+import photo2 from "./assets/images/profile_pic2.jpg";
+
+export const photos = [photo, photo2];
 
 export const tools = [
   { label: "HTML5", icon: <DiHtml5 />, color: "#e96228" },
   { label: "CSS3", icon: <DiCss3 />, color: "#3a97d0" },
   { label: "Sass", icon: <DiSass />, color: "#c76494" },
-  { label: "Tailwind CSS", icon: <SiTailwindcss />, color: "#07b0ce" },
-  { label: "JavaScript", icon: <DiJsBadge />, color: "#f7df1e" },
+  { label: "Tailwind", icon: <SiTailwindcss />, color: "#07b0ce" },
+  { label: "JavaScipt", icon: <DiJsBadge />, color: "#f7df1e" },
   { label: "React", icon: <DiReact />, color: "#61dbfb" },
-  { label: "SQL", icon: <TbDatabase />, color: "#e6792f" },
   { label: "Typescript", icon: <SiTypescript />, color: "#3178c6" },
+  { label: "Express", icon: <SiExpress />, color: "#000000" },
   { label: "Python", icon: <DiPython />, color: "#4584b6" },
   { label: "Python", icon: <DiPython />, color: "#ffde57" },
   { label: "Django", icon: <DiDjango />, color: "#0c4b33" },
   { label: "MongoDB", icon: <SiMongodb />, color: "#00684a" },
-  { label: "Express", icon: <SiExpress />, color: "#000000" },
+  { label: "PostgreSQL", icon: <SiPostgresql />, color: "#0064a5" },
+  { label: "Adobe Suite", icon: <SiAdobe />, color: "#ed2224" },
 ];
 
 export const traits = [
-  "Action-oriented",
   "Lifelong Learner",
   "Autodidact",
   "Foodie",
